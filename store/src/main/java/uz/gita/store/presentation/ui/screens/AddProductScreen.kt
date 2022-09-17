c5dded6cbb3fc89ca272c809d8f803ae277f131a
Khayrullo <khayrullo.me@gmail.com>
2022-09-17 21:55:40 +0500

    -add add product page

--- store/src/main/java/uz/gita/store/presentation/ui/screens/AddProductScreen.kt
+++ store/src/main/java/uz/gita/store/presentation/ui/screens/AddProductScreen.kt
@@ -5,8 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.core.view.children
+import by.kirich1409.viewbindingdelegate.viewBinding
 import uz.gita.store.R
+import uz.gita.store.databinding.AddProductScreenBinding
 
 class AddProductScreen : Fragment(R.layout.add_product_screen) {
+    val viewBinding: AddProductScreenBinding by viewBinding(AddProductScreenBinding::bind)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        for(i in viewBinding.atributs.children){
+//        }
 
+    }
 }
---
+++ store/src/main/java/uz/gita/store/screens/CategoryScreen.kt
@@ -0,0 +1,7 @@
+package uz.gita.store.screens
+
+import androidx.fragment.app.Fragment
+import uz.gita.store.R
+
+class CategoryScreen :Fragment(R.layout.fragment_category_screen){
+}
