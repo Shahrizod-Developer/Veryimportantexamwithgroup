6ac86a2819e65eb49ff5b315c9dd37a98d2be544
Ulug'bek <usmonov11670122@gmail.com>
2022-09-17 19:23:47 +0500

    -add CategoryScreen

---
+++ client/src/main/java/uz/gita/client/presentation/ui/screens/CategoriesScreen.kt
@@ -0,0 +1,15 @@
+package uz.gita.client.presentation.ui.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import uz.gita.client.R
+
+
+
+class CategoriesScreen : Fragment(R.layout.fragment_categories_screen) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
