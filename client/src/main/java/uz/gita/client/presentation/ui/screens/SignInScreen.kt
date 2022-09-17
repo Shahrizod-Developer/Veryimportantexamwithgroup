f64b468d8e72ca57d8706778063f27ce05241729
madina227 <madina.ismoilova.227@gmail.com>
2022-09-17 20:10:21 +0500

    add fragments base sign in and sign up

--- client/src/main/java/uz/gita/client/presentation/ui/screens/SignInScreen.kt
+++ client/src/main/java/uz/gita/client/presentation/ui/screens/SignInScreen.kt
@@ -1,6 +1,15 @@
 package uz.gita.client.presentation.ui.screens
-import uz.gita.client.R
-import androidx.fragment.app.Fragment
 
-class SignInScreen:Fragment(R.layout.screen_sign_in) {
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import uz.gita.client.R
+import uz.gita.client.databinding.ScreenSignInBinding
+
+class SignInScreen : Fragment(R.layout.screen_sign_in) {
+    private val viewBinding: ScreenSignInBinding by viewBinding(ScreenSignInBinding::bind)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
