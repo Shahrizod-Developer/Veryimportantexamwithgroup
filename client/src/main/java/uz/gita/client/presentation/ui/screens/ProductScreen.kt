14e6b6763dfd5bb228860f4c765f083c019c12fc
Ro'ziMuhammad Xolxo'jayev <xolxujayevv@gmail.com>
2022-09-17 20:31:18 +0500

    product screen

---
+++ client/src/main/java/uz/gita/client/presentation/ui/screens/ProductScreen.kt
@@ -0,0 +1,12 @@
+package uz.gita.client.presentation.ui.screens
+
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import uz.gita.client.R
+
+
+class ProductScreen : Fragment(R.layout.fragment_product_screen) {
+
+    private lateinit var recyclerView: RecyclerView
+    private
+}
