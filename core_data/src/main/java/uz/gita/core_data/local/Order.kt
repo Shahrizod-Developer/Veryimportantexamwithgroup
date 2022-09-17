11703a40e5fee6dfc4b6cc5252c9469eaec99c3b
jamshidisoqov <isoqovjamshid01@gmail.com>
2022-09-17 19:36:41 +0500

    store data finished

---
+++ core_data/src/main/java/uz/gita/core_data/local/Order.kt
@@ -0,0 +1,7 @@
+package uz.gita.core_data.local
+
+// Created by Jamshid Isoqov an 9/17/2022
+data class Order(
+    val productData: ProductData,
+    val count: Int
+)
---
+++ core_data/src/main/java/uz/gita/core_data/local/ProductData.kt
@@ -0,0 +1,12 @@
+package uz.gita.core_data.local
+
+// Created by Jamshid Isoqov an 9/17/2022
+data class ProductData(
+    val id: String,
+    val categoryId: String,
+    val name: String,
+    val description: String,
+    val photos: List<String>,
+    val sell: String,
+    val attrs: List<Pair<String, String>>
+)
