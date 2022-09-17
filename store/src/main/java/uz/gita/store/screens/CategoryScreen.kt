c2a68659f796e8c557f62968992dcbbc471c34e7
ixtiy <ixtiyorjonisaocff@gmail.com>
2022-09-17 20:33:47 +0500

    category screendan otaydigon boldi addscreenga

--- store/src/main/java/uz/gita/store/screens/CategoryScreen.kt
+++ store/src/main/java/uz/gita/store/screens/CategoryScreen.kt
@@ -1,7 +1,20 @@
 package uz.gita.store.screens
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import uz.gita.store.R
+import uz.gita.store.databinding.FragmentCategoryScreenBinding
 
 class CategoryScreen :Fragment(R.layout.fragment_category_screen){
+    private val viewBinding: FragmentCategoryScreenBinding by viewBinding(FragmentCategoryScreenBinding::bind)
+    private val navController by lazy { findNavController() }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewBinding.addBtn.setOnClickListener{
+            navController.navigate(CategoryScreenDirections.actionCategoryScreenToAddCategoryScreen())
+        }
+    }
 }
