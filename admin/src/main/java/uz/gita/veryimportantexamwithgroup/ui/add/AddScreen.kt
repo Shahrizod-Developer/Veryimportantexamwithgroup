58d7c1c2ab8fe3dfd6f21c2e1f9381dbbe1af4d8
Hasanboy Abdusattorov <hasanboy.goo@gmail.com>
2022-09-17 22:00:02 +0500

    -add AddScreen and EditText, Button shapes

---
+++ admin/src/main/java/uz/gita/veryimportantexamwithgroup/ui/add/AddScreen.kt
@@ -0,0 +1,7 @@
+package uz.gita.veryimportantexamwithgroup.ui.add
+
+import androidx.fragment.app.Fragment
+import uz.gita.veryimportantexamwithgroup.R
+
+class AddScreen : Fragment(R.layout.screen_add) {
+}
